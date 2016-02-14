feab904f2ba3258d8c4e21bd8923a5f323518d69
lonnystrunk <lonny.strunk@gmail.com>
2016-02-14 03:39:07 +0000

    Updated main.cpp, we can set up topologies now with command-line arguments.

--- main.cpp
+++ main.cpp
@@ -25,9 +25,46 @@ using std::rand;
 #include <cmath>
 using std::tanh;
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        cout << "INCORRECT USAGE\nProper Usage:\n\t"<< argv[0] << " inputLayer [otherLayers] outputLayer"
+        << "\n\t(Requires at least two layer topologies as inputs)" << endl;
+    }
     
+    cout << "TOPOLOGY:\n";
+    for(int i=1; i<argc; ++i)
+    {
+        cout << "Layer " << i << ": " << argv[i] << " nodes\n";
+    }
+    cout << endl;
+    
+    vector<MatrixXXd> layers;
+    vector<MatrixXXd> weights;
+    
+    //cout << MatrixXXd(atoi(argv[1]),atoi(argv[2]))::Random() << endl;
+    
+    // Create one matrix for each layer topology given (minus 1)
+    for(int i=1; i<argc-1; ++i)
+    {
+        layers.push_back( MatrixXXd(1,atoi(argv[i])) );
+        
+        MatrixXXd temp(atoi(argv[i]),atoi(argv[i+1]));
+        temp.setRandom();
+        weights.push_back(temp);
+    }
+    layers.push_back( MatrixXXd(1,atoi(argv[argc-1])) );
+    
+    for(auto layer: layers)
+    {
+        //cout << "Thing:\n" << layer << endl;
+    }
+    
+    for(auto weight: weights)
+    {
+        //cout << "Thing:\n" << weight << endl;
+    }
     
     return 0;
 }
