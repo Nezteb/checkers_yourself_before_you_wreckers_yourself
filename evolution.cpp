0042338bc8c1c6b3b1c54b14aa293bc42c8b305f
Noah Betzen <nbetzen@hotmail.com>
2016-02-29 19:54:56 -0900

    Fixed evolution

--- evolution.cpp
+++ evolution.cpp
@@ -31,9 +31,12 @@ using std::tanh;
 #include <functional>
 using std::ptr_fun;
 #include <random>
+using std::random_device;
+using std::mt19937;
 using std::normal_distribution;
 
-
+random_device seed;
+mt19937 superRandom(seed());
 
 class NeuralNetwork
 {
@@ -45,28 +48,28 @@ public:
             _layers.push_back( MatrixXXd(1, _topology[i]) );
             _layers[i].setRandom();
         }
-    
+
         for(int i=0; i<_topology.size()-1; ++i) // weights
         {
             _weights.push_back( MatrixXXd(_topology[i], _topology[i+1]) );
             _weights[i].setRandom();
         }
     }
-    
+
     void feedForward()
     {
         for(int j=1; j<_layers.size(); ++j) // go through all the layers
         {
             _layers[j].noalias() = _layers[j-1] * _weights[j-1]; // next layer = previous layer * weights in between
 
-            _layers[j].noalias() = _layers[j];//.unaryExpr(ptr_fun(_sigmoid)); // apply sigmoid to layer
+            _layers[j].noalias() = _layers[j].unaryExpr(ptr_fun(&_sigmoid)); // apply sigmoid to layer
         }
     }
-    
+
     NeuralNetwork spawnChild()
     {
         NeuralNetwork childNetwork(_topology);
-        
+
         for(int i = 0; i < _weights.size(); ++i)
         {
             for(int row = 0; row < _weights[i].rows(); ++row)
@@ -75,16 +78,16 @@ public:
                 {
                     // create new distirubtion based on weight and sigma
                     normal_distribution<double> distribution(_weights[i](row,col), rand());
-                    
+
                     // apply distribution to weight
-                    childNetwork._weights[i](row,col) = distribution(_weights[i](row,col));
+                    childNetwork._weights[i](row,col) = distribution(superRandom);
                 }
             }
         }
-        
+
         return childNetwork;
     }
-    
+
 private:
     static double _sigmoid(double x)
     {
@@ -103,21 +106,21 @@ int main(int argc, char *argv[])
         cout << "INCORRECT USAGE\nProper Usage:\n\t"<< argv[0] << " inputLayer [otherLayers] outputLayer"
         << "\n\t(Requires at least two layer topologies as inputs)" << endl;
     }
-    
+
     srand(time(NULL));
-    
+
     vector<int> inputs;
-    
+
     for(int i=1; i<argc; ++i) // first input is program name
     {
         inputs.push_back(atoi(argv[i]));
     }
-    
+
     NeuralNetwork test(inputs);
-    
-    
-    
-    
-    
+
+
+
+
+
     return 0;
 }
